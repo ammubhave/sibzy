3b51790addd1fb272bb2aebfc1c50d77add1ea15
Amol Bhave <ammubhave@gmail.com>
2014-01-02 17:50:47 -0600

    make css work for res profile

--- sibzy/static/js/restaurant.js
+++ sibzy/static/js/restaurant.js
@@ -1,4 +1,17 @@
 $(function () {
-    path = document.location.substring(document.location.indexOf('#!') + 2)
+    path = document.location.hash.substring(2);//.substring(document.location.indexOf('#!') + 2)
+    paths = path.split('/')
+    ;
+    if (paths[1] == 'profile') {
+        $.ajax({
+            url: '/!/restaurant/profile/' + paths[2],
+            dataType: 'json',
+            success: function (data) {
+                console.log(data);
+                $('.restaurant-name').text(data.name);
+                
+            }
+        });
+    }
     console.log(path);
 });
