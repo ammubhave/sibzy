3c0359cb4d4d66382a3f18196e291365548a77fd
Amol Bhave <ammubhave@gmail.com>
2014-01-08 14:27:27 -0600

    add adming

--- sibzy/restaurant/admin.py
+++ sibzy/restaurant/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from restaurant.models import Restaurant, Location, RestaurantRating, City, State, Country
+from restaurant.models import Restaurant, RestaurantCategory, Location, RestaurantRating, City, State, Country
 
 
 admin.site.register(Restaurant)
+admin.site.register(RestaurantCategory)
 admin.site.register(Location)
 admin.site.register(RestaurantRating)
 admin.site.register(City)
