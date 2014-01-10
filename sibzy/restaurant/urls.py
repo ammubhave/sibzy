3cbbbc65f6e32320bee9d21eff6c3837456f6301
Jonathan Uesato <juesato@mit.edu>
2014-01-10 11:13:16 -0500

    updating urls.py

--- sibzy/restaurant/urls.py
+++ sibzy/restaurant/urls.py
@@ -7,7 +7,6 @@ admin.autodiscover()
 urlpatterns = patterns(
     '',
     url(r'^load/profile', 'frontend.views.load_template', {'template_path': 'restaurant_profile'}),
-
     url(r'^profile/(.*)$', 'restaurant.views.profile'),
     url(r'^initdb', 'restaurant.views.fill_locations'),
     url(r'^scrape', 'restaurant.views.test_view')
--- sibzy/restaurant/views.py
+++ sibzy/restaurant/views.py
@@ -11,95 +11,7 @@ import urllib2
 import re
 import contextlib
 
-text = urllib2.urlopen("https://www.grubhub.com/search/Cambridge,_MA/").read()
-
-urls = re.findall('data-restaurant-id="(.*?)"', text)
-p = re.compile('<li class="item.*?<span class="name.*?">([a-zA-Z0-9 \']*?)</span>\s*<span class="price"><span class="dollarSign">\$</span>(.*?)</span>', flags = re.DOTALL)
-urls.pop(1)
-
-def printdishes(s):
-    dishes = p.findall(s)
-        #for dish in dishes:
-        #    dish[0] + "', '" + dish[1]
-def populateFromGrubhub():
-    for url in urls:
-        #print url
-        listing = []
-        with contextlib.closing(urllib2.urlopen("https://www.grubhub.com/restaurant/" + url)) as spage:
-            s = spage.read()
-            name = re.search('<div itemprop="name">(.*?)</div>', s)
-            #address = re.search('<p class="restaurantAddress fine_print">(.*?)</p>', s)
-            #print "Address:", address.group(1)
-            street = re.search('<span itemprop="streetAddress">(.*?)</span>', s)
-            city = re.search('<span itemprop="addressLocality">(.*?)</span>', s)
-            state = re.search('<span itemprop="addressRegion">(.*?)</span>', s)
-            zipcode = re.search('<span itemprop="postalCode">(.*?)</span>', s)
-            pickup = 'data-mp-with-pickup-available="true"' in s
-            telephone = re.search('<div itemprop="telephone">(.*?)</div>', s)
-            latitude = re.search('<meta itemprop="latitude" content="(.*?)"', s)
-            longitude = re.search('<meta itemprop="longitude" content="(.*?)"', s)
-            cuisine = re.findall('<span itemprop="servesCuisine">(.*?)</span>', s)
-            # if (city.group(1) )
-            loc = Location(latitude = latitude.group(1), longitude = longitude.group(1), address = street.group(1), city = City.objects.get(name = 'Boston'), state = State.objects.get(name = 'Massachusetts'), country = Country.objects.get(name = "United States"), phone = telephone.group(1))
-            loc.save()
-            dishes = p.findall(s) #dish menu
-            sampledishlist = []
-            for dish in dishes:
-                # categ = DishCategory(name = 'a', slug = 'b')
-                categ = DishCategory.objects.filter(slug = 'b')
-                if len(categ) == 0:
-                    categ = DishCategory(name = 'a', slug = 'b')
-                    categ.save()
-                else:
-                    categ = categ[0]
-                dish = Dish(name = dish[0], tag = dish[0], price = dish[1].rstrip('+') )
-                #dish = Dish(name = dish[0], tag = dish[0], price = dish[1].rstrip('+'), vegetarian = len(dish[0])%2, vegan = len(dish[0])%2, gluten = len(dish[0])%2 )
-                dish.save()
-                dish.categories.add(categ)
-                dish.save()
-                sampledishlist.append(dish)
-
-            rating = RestaurantRating(total = 1, vegetarian = 1, vegan = 1, glutenfree = 1, peanutfree = 1, lactoseint = 1, seafoodint = 1)
-            rating.save()
-            cool = Restaurant(name = name.group(1), location = loc, rating= rating)
-            cool.save()
-            # print name.group(1)
-            for dish in sampledishlist:
-                cool.dishes.add(dish)
-            cool.save()
-            #dishes = p.findall(s)
-            #for dish in dishes:
-            #    dish[0] + "', '" + dish[1]
-
-            #printdishes(s)
-            
-def test_view(request):
-    #
-    #l = Location(lati, l , )
-    #rr = RestaurantRating(total=0, ve= -0 , sd= 0)
-    #r = Restaurant(name=, location=l, rating=rr, )
-    #r.save()
-    populateFromGrubhub()
-    return HttpResponse("OK")
-
-def profile(request, restaurant_id):
-    ''' Return the restaurant object corresponsing to restaurant_id
-
-    **Arguments:**
-        *restaurant_id*: The numeric ID of the restaurant
-
-    **Returns:**
-        ``<restaurant JSON object>``
-
-    '''
-
-
-    restaurant = Restaurant.objects.get(id=restaurant_id)
-
-    response = HttpResponse(restaurant.json())
-    return response
-
-<<<<<<< HEAD
+# initialize database
 def fill_locations(request):
 
     country_usa = Country(name='United States')
@@ -172,7 +84,77 @@ def fill_locations(request):
             lexington.save()
 
     return HttpResponse("OK")
-=======
-def methodhere(request):
-    pass
->>>>>>> e01db84f1763a7bb8d48cfc598bb08009e063d16
+
+
+#Grubhub scraper for Cambridge area
+text = urllib2.urlopen("https://www.grubhub.com/search/Cambridge,_MA/").read()
+
+urls = re.findall('data-restaurant-id="(.*?)"', text)
+p = re.compile('<li class="item.*?<span class="name.*?">([a-zA-Z0-9 \']*?)</span>\s*<span class="price"><span class="dollarSign">\$</span>(.*?)</span>', flags = re.DOTALL)
+urls.pop(1)
+
+def printdishes(s):
+    dishes = p.findall(s)
+def populateFromGrubhub():
+    for url in urls:
+        listing = []
+        with contextlib.closing(urllib2.urlopen("https://www.grubhub.com/restaurant/" + url)) as spage:
+            s = spage.read()
+            name = re.search('<div itemprop="name">(.*?)</div>', s)
+            #address = re.search('<p class="restaurantAddress fine_print">(.*?)</p>', s)
+            #print "Address:", address.group(1)
+            street = re.search('<span itemprop="streetAddress">(.*?)</span>', s)
+            city = re.search('<span itemprop="addressLocality">(.*?)</span>', s)
+            state = re.search('<span itemprop="addressRegion">(.*?)</span>', s)
+            zipcode = re.search('<span itemprop="postalCode">(.*?)</span>', s)
+            pickup = 'data-mp-with-pickup-available="true"' in s
+            telephone = re.search('<div itemprop="telephone">(.*?)</div>', s)
+            latitude = re.search('<meta itemprop="latitude" content="(.*?)"', s)
+            longitude = re.search('<meta itemprop="longitude" content="(.*?)"', s)
+            cuisine = re.findall('<span itemprop="servesCuisine">(.*?)</span>', s)
+            loc = Location(latitude = latitude.group(1), longitude = longitude.group(1), address = street.group(1), city = City.objects.get(name = 'Boston'), state = State.objects.get(name = 'Massachusetts'), country = Country.objects.get(name = "United States"), phone = telephone.group(1))
+            loc.save()
+            dishes = p.findall(s) #dish menu
+            sampledishlist = []
+            for dish in dishes:
+                categ = DishCategory.objects.filter(slug = 'b')
+                if len(categ) == 0:
+                    categ = DishCategory(name = 'a', slug = 'b')
+                    categ.save()
+                else:
+                    categ = categ[0]
+                dish = Dish(name = dish[0], tag = dish[0], price = dish[1].rstrip('+') )
+                #dish = Dish(name = dish[0], tag = dish[0], price = dish[1].rstrip('+'), vegetarian = len(dish[0])%2, vegan = len(dish[0])%2, gluten = len(dish[0])%2 )
+                dish.save()
+                dish.categories.add(categ)
+                dish.save()
+                sampledishlist.append(dish)
+
+            rating = RestaurantRating(total = 1, vegetarian = 1, vegan = 1, glutenfree = 1, peanutfree = 1, lactoseint = 1, seafoodint = 1)
+            rating.save()
+            cool = Restaurant(name = name.group(1), location = loc, rating= rating)
+            cool.save()
+            # print name.group(1)
+            for dish in sampledishlist:
+                cool.dishes.add(dish)
+            cool.save()          
+def test_view(request):
+    populateFromGrubhub()
+    return HttpResponse("OK")
+
+
+def profile(request, restaurant_id):
+    ''' Return the restaurant object corresponsing to restaurant_id
+
+    **Arguments:**
+        *restaurant_id*: The numeric ID of the restaurant
+
+    **Returns:**
+        ``<restaurant JSON object>``
+
+    '''
+
+    restaurant = Restaurant.objects.get(id=restaurant_id)
+
+    response = HttpResponse(restaurant.json())
+    return response
