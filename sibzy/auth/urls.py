b053e4ae6b9d611f6b6b7f2663b3252f7836d17c
Fernando Trujano <trujano@Fernandos-MacBook-Pro.local>
2014-01-10 17:49:27 -0500

    added survey

--- sibzy/auth/urls.py
+++ sibzy/auth/urls.py
@@ -9,4 +9,5 @@ urlpatterns = patterns(
     url(r'^login/fb$', 'auth.views.login_fb'),
     url(r'^logout$', 'auth.views.logout_fb'),
     url(r'^load/me$', 'auth.views.me'),
+    url(r'^load/survey$', 'frontend.views.load_template', {'template_path': 'auth_survey'}),
 )
